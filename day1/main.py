b5297c5fd0ae312af60175f71044871e45ed6de4
William Guay <guay.william01@gmail.com>
2024-12-01 00:26:45 -0500

    refactoring

--- day1/main.py
+++ day1/main.py
@@ -16,15 +16,11 @@ ans = 0
 for x in range(len(l1)):
     ans += abs(l1[x] - l2[x])
     
-print("Partie 1: " + ans)
+print("Partie 1: " + str(ans))
 
 ans2 = 0
 
 for x in l1:
-    nb = 0
-    for y in l2:
-        if x == y:
-            nb+=1
-    ans2 += nb * x
+    ans2 += l2.count(x) * x
     
-print("Partie 2: " + ans2)
+print("Partie 2: " + str(ans2))
